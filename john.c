544f1bc6a416b84e28604d7a615dfa66b8a3aa62
littlejiangyuan <jiangyuanbj@aliyun.com>
2016-07-09 04:23:55 -0700

    add

--- john.c
+++ john.c
@@ -16,18 +16,51 @@
 
 #include "john.h"
 
+ZEND_BEGIN_ARG_INFO(byref_compiletime_arginfo, 0)
+	ZEND_ARG_INFO(1, 'a')
+	ZEND_ARG_INFO(0, 'b')
+	ZEND_ARG_ARRAY_INFO(0, 'c', 1)
+ZEND_END_ARG_INFO()
 
+//声明导出函数快
 static zend_function_entry john_functions[] = {
-    PHP_FE(walu_hello,        NULL)
+    PHP_FE(para_ref,        NULL)
+	PHP_FE(comp_ref,        byref_compiletime_arginfo)
     { NULL, NULL, NULL }
 };
 
-ZEND_FUNCTION(walu_hello)
+ZEND_FUNCTION(para_ref)
 {
-	ZVAL_LONG(return_value, 42);
+
 }
 
-//module entry
+/*************chapter 2**************/
+ZEND_FUNCTION(comp_ref)
+{
+	zval *a,*c;
+	long b;
+
+	//我们我接收的参数传给zval *a;
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "zla", &a, &b, &c) == FAILURE)
+    {
+		RETURN_NULL();
+	}
+
+	long tmp = 0;
+	tmp = Z_LVAL_P(a) + b;
+	Z_LVAL_P(a) = tmp;
+
+	array_init(return_value);
+
+	//zend_hash_copy( Z_ARRVAL_P(return_value), Z_ARRVAL_P(c), NULL, NULL, sizeof(zval *) );
+	add_next_index_long(c, tmp );
+	Z_ARRVAL_P(return_value) = Z_ARRVAL_P(c);
+
+	return;
+}
+
+
+//填写模块信息
 zend_module_entry john_module_entry = {
 #if ZEND_MODULE_API_NO >= 20010901
      STANDARD_MODULE_HEADER,
